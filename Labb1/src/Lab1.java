6b1b727b88ffdc3c43f907bb446528be12ae16f9
BlixterinoProductions <max.blixt@gmail.com>
2017-01-18 15:32:03 +0100

    2

--- Labb1/src/Lab1.java
+++ Labb1/src/Lab1.java
@@ -3,10 +3,11 @@
  */
 public class Lab1 {
     public static void sort(int[] a) {
-  ...
+
     }
 
     public static void main(String[] args) throws Exception {
-  ...
+        byte[] bs = java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(args[0]));
+
     }
 }
