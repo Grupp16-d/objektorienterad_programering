1ed265134cc8d528b93e34969f2e8fa5352c158e
BlixterinoProductions <max.blixt@gmail.com>
2017-01-18 16:19:38 +0100

    Merge remote-tracking branch 'origin/master'

--- Labb1/src/Lab1.java
+++ Labb1/src/Lab1.java
@@ -1,15 +1,13 @@
-/**
- * Created by Max on 18/01/17.
- */
 public class Lab1 {
     public static void sort(int[] a) {
-        for (int i = a.length - 1; i > 0; i--) { 
-            for (int j = 0; j < i; j++) { 
-                if (a[j] - a[j+1] > 0) { 
-                    int temp = a[j];
+        int temp;
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] - a[j+1] > 0){
+                    temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] = temp;
-                } 
+                }
             }
         }
     }
@@ -17,7 +15,6 @@ public class Lab1 {
     public static void main(String[] args) throws Exception {
         int[] x = {6,2,1,10,7};
         sort(x);
-        System.out.println(x);
+        System.out.println(x[0]);
     }
 }
-
--- Labb1/src/Main.java
+++
@@ -1,9 +0,0 @@
-//Labb 1 av Oliver Gustavsson & Max Blixt
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-
-
-}
