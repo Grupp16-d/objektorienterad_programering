646f7b7b9d771e7c7125a28a0736f0f31e42e639
BlixterinoProductions <max.blixt@gmail.com>
2017-01-18 15:56:37 +0100

    dfuckangioäsdjlgm,

--- Labb1/src/Lab1.java
+++ Labb1/src/Lab1.java
@@ -3,11 +3,21 @@
  */
 public class Lab1 {
     public static void sort(int[] a) {
-
+        for (int i = a.length - 1; i > 0; i--) { 
+            for (int j = 0; j < i; j++) { 
+                if (a[j] - a[j+1] > 0) { 
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                } 
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
-        byte[] bs = java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(args[0]));
-
+        int[] x = {6,2,1,10,7};
+        sort(x);
+        System.out.println(x);
     }
 }
+
