59433b9fc25bd5478bae3aa63f956d879172e80f
BlixterinoProductions <max.blixt@gmail.com>
2017-01-24 17:27:10 +0100

    RayNum

--- Labb2/src/RatNum.java
+++ Labb2/src/RatNum.java
@@ -1,10 +1,24 @@
-
-
 public class RatNum {
-    public static int sgd(int m, int n){
+   // public static int sgd(int m, int n){
+      public static int sgd(int m, int n){
         if(m==0 || n==0) {
             throw new IllegalArgumentException();
         }
+
+        int absM = Math.abs(m);
+        int absN = Math.abs(n);
+
+        int r = 1;
+        while(r != 0){
+            r = absM / absN;
+            absM = absN;
+            absN = r;
+        }
+
+        m = m / absN;
+        n = n / absN;
+        /*--------------------------------- FIXA RETURN -----------------------*/
+        return m, n;
     }
     //Steg 2
 
@@ -24,3 +38,9 @@ public class RatNum {
     //
 
 }
+
+return new MyResult(number1, number2);
+
+    Dividera m med n och låt r vara resten vid divisionen.
+    Om r = 0 så är beräkningen klar och resultatet finns i n.
+    Sätt annars m till n och n till r och gå tillbaka till steg 1.
