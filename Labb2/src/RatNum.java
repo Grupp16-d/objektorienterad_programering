7a6b97d5df2a215b6e82baa14f0c79921020ad9c
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-01-24 17:26:05 +0100

    ett försök i allfall

--- Labb2/src/RatNum.java
+++ Labb2/src/RatNum.java
@@ -6,4 +6,21 @@ public class RatNum {
             throw new IllegalArgumentException();
         }
     }
+    //Steg 2
+
+    //steg 3
+    //En metod toString som returnerar det aktuella talet som en text
+    private toString(){
+
+    }
+
+    //beräkna ett närmevärde på n/m
+    private double toDouble(double m, double n){
+        double x;
+        x = m/n;
+        return x;
+    }
+
+    //
+
 }
