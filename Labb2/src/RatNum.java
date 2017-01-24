e38cc622816fbb9d53f045f75e6cefc0b6b3eecf
BlixterinoProductions <max.blixt@gmail.com>
2017-01-24 16:57:44 +0100

    hej fuckingdas

--- Labb2/src/RatNum.java
+++ Labb2/src/RatNum.java
@@ -2,13 +2,8 @@
 
 public class RatNum {
     public static int sgd(int m, int n){
-<<<<<<< HEAD
         if(m==0 || n==0) {
             throw new IllegalArgumentException();
         }
-=======
-
-
->>>>>>> origin/master
     }
 }
