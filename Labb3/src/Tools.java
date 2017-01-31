fc7689585b713410ac54537a1d5def73a5d212e2
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-01-31 14:42:04 +0100

    get vettigt hat hänt

--- Labb3/src/Tools.java
+++ Labb3/src/Tools.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
+import java.util.Random;
+import javax.swing.*;
 
-public class randomOrder {
+public class Tools {
+    Void randomOrder{
 
+    }
 }
+
+
+/*
+        public void main(String[]args) {
+            int[] tempArray = new int[5];
+            for(int i = 0; i < tempArray.length; i++) {
+                tempArray[i] = (int)(Math.random()*0+9);
+            }
+
+        }*/
