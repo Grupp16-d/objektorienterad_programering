5b074045a10686ce2bbb9ddce04e22d6841c98c7
BlixterinoProductions <max.blixt@gmail.com>
2017-01-31 14:48:52 +0100

    cards

--- Labb3/src/card.java
+++ Labb3/src/card.java
@@ -1,2 +1,33 @@
-public class card {
+import javax.swing.*;
+
+public class Card extends JButton{
+
+    public enum Status {
+        HIDDEN, VISIBLE, MISSING
+    }
+
+    public Status status;
+    public Icon picture;
+
+    public Card(Icon x){
+        this.status = Status.MISSING;
+        this.picture = x;
+    }
+
+    public Card(Icon x, Status s){
+        this.status = s;
+        this.picture = x;
+    }
+
+    public void setStatus(Status s){
+        this.status = s;
+    }
+
+    public Status getStatus(){
+        return status;
+    }
+
+    public boolean sameIcon(Card b){
+        
+    }
 }
