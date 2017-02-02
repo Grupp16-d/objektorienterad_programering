17dacd8ded47459cc9128d4e23ff872f14bef865
BlixterinoProductions <max.blixt@gmail.com>
2017-02-02 15:31:06 +0100

    memory

--- Labb3/src/Memory.java
+++ Labb3/src/Memory.java
@@ -1,4 +1,16 @@
 import javax.swing.*;
+import java.io.File;
+import java.io.ObjectInput;
 
 public class Memory {
+    File folder = new File("CardImages");
+    File[] pictures = folder.listFiles();
+    Object[] imageIcon = new Object[pictures.length];
+
+    public Memory(){
+        for(int i = 0; i < pictures.length; i++){
+            imageIcon[i] = new ImageIcon(pictures[i].getPath());
+        }
+    }
+
 }
