8c8f8cae3837e4386b6bd5daafdd5240ee963215
BlixterinoProductions <max.blixt@gmail.com>
2017-02-02 16:37:16 +0100

    new game

--- Labb3/src/Memory.java
+++ Labb3/src/Memory.java
@@ -1,16 +1,31 @@
 import javax.swing.*;
 import java.io.File;
-import java.io.ObjectInput;
 
 public class Memory {
     File folder = new File("CardImages");
     File[] pictures = folder.listFiles();
-    Object[] imageIcon = new Object[pictures.length];
+    Icon[] imageIcon = new Icon[pictures.length];
 
     public Memory(){
         for(int i = 0; i < pictures.length; i++){
             imageIcon[i] = new ImageIcon(pictures[i].getPath());
         }
+    }
+
+    public void newGame(int n){
+        Icon[] randomArray = new Icon[imageIcon.length];
+        System.arraycopy(imageIcon, 0, randomArray, 0, randomArray.length);
+        Tools.randomOrder(randomArray);
+        Object[] cards = new Icon[n];
+
+        for(int i = 0; i < n/2; i++){
+            for(int j = 0; j < n; j = j + 2){
+                cards[j] = new Card(randomArray[i]);
+                cards[j+1] = new Card(randomArray[i]);
+            }
+        }
+
+        Tools.randomOrder(cards);
     }
 
 }
