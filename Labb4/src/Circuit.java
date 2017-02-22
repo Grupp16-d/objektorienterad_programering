a6f634367c33b1a680d9aed17449344880a5d2ab
BlixterinoProductions <max.blixt@gmail.com>
2017-02-22 20:03:25 +0100

    buggy mess

--- Labb4/src/Circuit.java
+++ Labb4/src/Circuit.java
@@ -8,58 +8,60 @@ public class Circuit {
 
     private Map<String, CircuitComponent> components = new HashMap<>();
 
-    public Circuit() {}
+    public Circuit() {
+    }
 
     public Circuit(List<String> lines) {
         for (String line : lines) {
             String[] tempSplit = line.split("\\s+");
-            if (tempSplit[1] == "INPUT") {
+            if (tempSplit[1].equals("INPUT")) {
                 CircuitComponent comp = new Input(false);
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "ZERO") {
+            } else if (tempSplit[1].equals("ZERO")) {
                 CircuitComponent comp = new Constant(false);
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "ONE") {
+            } else if (tempSplit[1].equals("ONE")) {
                 CircuitComponent comp = new Constant(true);
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "FORK") {
+            } else if (tempSplit[1].equals("FORK")) {
                 CircuitComponent comp = new Fork(2);
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "AND") {
+            } else if (tempSplit[1].equals("AND")) {
                 CircuitComponent comp = new AndGate();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "OR") {
+            } else if (tempSplit[1].equals("OR")) {
                 CircuitComponent comp = new OrGate();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "XOR") {
+            } else if (tempSplit[1].equals("XOR")) {
                 CircuitComponent comp = new XorGate();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "NOT") {
+            } else if (tempSplit[1].equals("NOT")) {
                 CircuitComponent comp = new NotGate();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "ADDER") {
+            } else if (tempSplit[1].equals("ADDER")) {
                 CircuitComponent comp = new Adder();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "OUTPUT") {
+            } else if (tempSplit[1].equals("OUTPUT")) {
                 CircuitComponent comp = new Output();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "DFLIPFLOP") {
+            } else if (tempSplit[1].equals("DFLIPFLOP")) {
                 CircuitComponent comp = new DFlipFlop();
                 addComponent(tempSplit[0], comp);
-            } else if (tempSplit[1] == "JKFLIPFLOP") {
+            } else if (tempSplit[1].equals("JKFLIPFLOP")) {
                 CircuitComponent comp = new JKFlipFlop();
                 addComponent(tempSplit[0], comp);
             }
-
         }
         for (String line : lines) {
             String[] tempSplit = line.split("\\s+");
-            for (int i = 2; i < tempSplit.length; i = i + 3) {
-                int outputIndex = Integer.parseInt(tempSplit[i]);
-                CircuitComponent receiver = getComponent(tempSplit[i + 1]);
-                int receiverIndex = Integer.parseInt(tempSplit[i + 2]);
+            if (tempSplit.length > 2) {
+                for (int i = 2; i < tempSplit.length; i = i + 3) {
+                    int outputIndex = Integer.parseInt(tempSplit[i]);
+                    CircuitComponent receiver = getComponent(tempSplit[i + 1]);
+                    int receiverIndex = Integer.parseInt(tempSplit[i + 2]);
 
-                getComponent(tempSplit[0]).connect(outputIndex, receiver, receiverIndex);
+                    getComponent(tempSplit[0]).connect(outputIndex, receiver, receiverIndex);
+                }
             }
         }
     }
@@ -75,9 +77,9 @@ public class Circuit {
     }
 
     public CircuitComponent getComponent(String name) {
-        if (components.get(name) != null){
+        if (components.get(name) != null) {
             return components.get(name);
-        }else{
+        } else {
             return null;
         }
     }
